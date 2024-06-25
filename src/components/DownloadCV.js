22748fab27e9a91f9444f18594067d8af9afbd12
Louay Jrad <jrad.louay@outlook.com>
2024-06-25 17:13:56 +0200

    remove route handler

--- src/components/DownloadCV.js
+++ src/components/DownloadCV.js
@@ -1,33 +1,17 @@
 "use client";
 
 function DownloadCV() {
-  async function downloadCV() {
-    try {
-      const response = await fetch("/api", {
-        responseType: "blob", // Important for binary data
-      });
-
-      // Extract filename from content-disposition header
-      const contentDisposition = response.headers["content-disposition"];
-      const fileNameMatch = contentDisposition.match(/filename="(.+)"/);
-      const fileName = fileNameMatch ? fileNameMatch[1] : "downloadedFile";
-
-      // Create a temporary anchor element to trigger the download
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement("a");
-      link.href = url;
-      // Setting filename received in response
-      link.setAttribute("download", fileName);
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    } catch (error) {
-      console.error("Error downloading file:", error);
-    }
+  async function handleDownload() {
+    const link = document.createElement("a");
+    link.href = "/Louay_Jrad_CV.pdf";
+    link.download = "Louay_Jrad_CV.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }
   return (
     <button
-      onClick={downloadCV}
+      onClick={handleDownload}
       className="self-start text-black text-[26px] font-semibold underline hover:text-violet-400"
     >
       Download my cv
