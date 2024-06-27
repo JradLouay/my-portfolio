5afc02bf5ff00817f5acec10d9bb9d81ab72babf
JradLouay <56163483+JradLouay@users.noreply.github.com>
2024-06-27 20:44:49 +0100

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,7 +4,7 @@ function HeroSection() {
   return (
     <header className="mt-28 px-0 md:px-14 pt-1 pb-14 mb-52">
       <div className="flex-col mb-8 justify-center items-start gap-5 flex">
-        <h2 className="text-7xl">Hi. I'm Louay.</h2>
+        <h2 className="text-7xl">Hi. I&aposm Louay.</h2>
         <p className="leading-[1.9em]">
           Software Engineer / Frontend Developer
           <br />
